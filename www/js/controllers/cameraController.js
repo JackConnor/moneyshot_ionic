ddf496c4b7a8e8b684858183262b9741a2e0f2fa
Jack Connor <jack.connor83@gmail.com>
2016-02-20 19:04:04 -0800

    added new testing module

--- www/js/controllers/cameraController.js
+++ www/js/controllers/cameraController.js
@@ -5,6 +5,7 @@ angular.module('cameraController', [])
   cameraCtrl.$inject = ['$http', '$scope', 'Camera'];
   function cameraCtrl($http, $scope, Camera){
     var self = $scope;
+    console.log('yoyoyyoyo');
     Camera.getPicture()
       .then(function(imageUrl){
         console.log(imageUrl);
