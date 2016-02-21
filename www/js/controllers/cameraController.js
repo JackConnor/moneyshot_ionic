6454d6494506768c000a2cfa514c602f6ed5ca8c
Jack Connor <jack.connor83@gmail.com>
2016-02-20 19:49:48 -0800

    connected camera to the backend, trying to get the first callback

--- www/js/controllers/cameraController.js
+++ www/js/controllers/cameraController.js
@@ -2,12 +2,17 @@ angular.module('cameraController', [])
 
   .controller('cameraCtrl', cameraCtrl);
 
-  cameraCtrl.$inject = ['$http', '$scope', 'Camera'];
-  function cameraCtrl($http, $scope, Camera){
+  cameraCtrl.$inject = ['$http', '$scope', '$cordovaFileTransfer', 'Camera'];
+  function cameraCtrl($http, $scope, $cordovaFileTransfer, Camera){
     var self = $scope;
     console.log('yoyoyyoyo');
     navigator.camera.getPicture(function(imageURI){
       console.log(imageURI);
+      var options = {}
+      $cordovaFileTransfer.upload('http://localhost:5555/api/newimage', imageURI, options, true)
+      .then(function(uploadResult){
+        console.log(uploadResult);
+      })
     })
     // Camera.getPicture()
     //   .then(function(imageUrl){
