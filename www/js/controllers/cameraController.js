1329ee88c9555bb264047bd26ee253e15d78a8c7
Jack Connor <jack.connor83@gmail.com>
2016-02-20 20:05:45 -0800

    let's try again

--- www/js/controllers/cameraController.js
+++ www/js/controllers/cameraController.js
@@ -6,26 +6,25 @@ angular.module('cameraController', [])
   function cameraCtrl($http, $scope, $cordovaFileTransfer, Camera){
     var self = $scope;
     console.log('yoyoyyoyo');
-    navigator.camera.getPicture(function(imageURI){
-      console.log(imageURI);
-      var options = {}
-      // $cordovaFileTransfer.upload('http://localhost:5555/api/newimage', imageURI, options)
-      // .then(function(uploadResult){
-      //   console.log(uploadResult);
-      // })
-      /////little test
-      function testApi(){
-        $http({
-          method: "GET"
-          ,url: "http://localhost:5555/api/test"
-        })
-        .then(function(data){
-          console.log(data.data);
-          console.log(data);
-        })
-      }
-      testApi();
-    })
+    function testApi(){
+      $http({
+        method: "GET"
+        ,url: "http://localhost:5555/api/test"
+      })
+      .then(function(data){
+        console.log(data.data);
+        console.log(data);
+      })
+    }
+    testApi();
+    // navigator.camera.getPicture(function(imageURI){
+    //   console.log(imageURI);
+    //   var options = {}
+    //   $cordovaFileTransfer.upload('http://localhost:5555/api/newimage', imageURI, options)
+    //   .then(function(uploadResult){
+    //     console.log(uploadResult);
+    //   })
+    // })
     // Camera.getPicture()
     //   .then(function(imageUrl){
     //     console.log(imageUrl);
