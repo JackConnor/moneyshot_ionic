63cff3948a9dd2a3e84f3edc515eea66d26565f5
Jack Connor <jack.connor83@gmail.com>
2016-02-22 11:41:01 -0800

    changed upload schema to use ngFileUpload

--- www/js/factories/takesinglephotofactory.js
+++ www/js/factories/takesinglephotofactory.js
@@ -2,18 +2,15 @@ angular.module('singlePhotoFactory', ['ngFileUpload'])
 
   .factory('singlePhoto', singlePhoto);
 
-  singlePhoto.$inject = ['$scope', '$http', 'Upload'];
+  singlePhoto.$inject = ['$http', 'Upload'];
 
-  function singlePhoto($scope, $http, Upload){
+  function singlePhoto($http, Upload){
     console.log(Upload);
     console.log('yoyoyo');
     function uploadPhoto(photoFile){
-      $scope.file = photoFile
-      return $http({
-        method: "POST"
+      return Upload.upload({
         ,url: "http://192.168.0.11:5555/api/newimage"
         ,data: {file: photoFile, text: "testing this out"}
-        ,files: [$scope.file]
       })
     }
     return uploadPhoto
