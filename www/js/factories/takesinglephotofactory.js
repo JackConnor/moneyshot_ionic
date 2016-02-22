679a2694fc21725319ba461f801be43b2b8aacd3
Jack Connor <jack.connor83@gmail.com>
2016-02-22 11:37:25 -0800

    new photo upload idea

--- www/js/factories/takesinglephotofactory.js
+++ www/js/factories/takesinglephotofactory.js
@@ -2,16 +2,18 @@ angular.module('singlePhotoFactory', ['ngFileUpload'])
 
   .factory('singlePhoto', singlePhoto);
 
-  singlePhoto.$inject = ['$http', 'Upload'];
+  singlePhoto.$inject = ['$scope', '$http', 'Upload'];
 
-  function singlePhoto($http, Upload){
+  function singlePhoto($scope, $http, Upload){
     console.log(Upload);
     console.log('yoyoyo');
     function uploadPhoto(photoFile){
+      $scope.file = photoFile
       return $http({
         method: "POST"
         ,url: "http://192.168.0.11:5555/api/newimage"
-        ,data: photoFile
+        ,data: {file: photoFile, text: "testing this out"}
+        ,files: [$scope.file]
       })
     }
     return uploadPhoto
