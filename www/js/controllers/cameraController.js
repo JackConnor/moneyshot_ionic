2abbcd6fe1a480c3fd85b230b29342f6c84b6d44
Jack Connor <jack.connor83@gmail.com>
2016-02-22 15:17:38 -0800

    removed a lot of phtoo stuff from the controller

--- www/js/controllers/cameraController.js
+++ www/js/controllers/cameraController.js
@@ -10,17 +10,6 @@ angular.module('cameraController', ['singlePhotoFactory', 'ngFileUpload'])
     $scope.testFile;
     console.log(singlePhoto);
     console.log('yoyoyyoyo');
-    function testApi(){
-      $http({
-        method: "GET"
-        ,url: "http://192.168.0.11:5555/api/test"
-      })
-      .then(function(data){
-        // console.log(data.data);
-        // console.log(data);
-      })
-    }
-    testApi();
     var options = {
         quality : 80,
         destinationType : Camera.DestinationType.FILE_URI,
@@ -38,7 +27,9 @@ angular.module('cameraController', ['singlePhotoFactory', 'ngFileUpload'])
       $cordovaFileTransfer.upload('http://192.168.0.11:5555/api/newimage', result, {})
       .then(function(callbackImage){
         console.log('in the callback');
-        console.log(callbackImage);
+        console.log(callbackImage.url);
+        // $scope.newPhoto = callbackImage.secure_url;
+        $('.testPhoto').attr('src', callbackImage.secure_url);
       })
     })
     ///////////////////////////////////
@@ -64,3 +55,11 @@ angular.module('cameraController', ['singlePhotoFactory', 'ngFileUpload'])
     //////////////end upload photos////
     ///////////////////////////////////
   }
+
+
+  // {
+  //   "responseCode":200,
+  //   "headers":
+  //     {"Access-Control-Allow-Origin":"*","Content-Type":"application/json; charset=utf-8","X-Powered-By":"Express","Connection":"keep-alive","Date":"Mon, 22 Feb 2016 23:10:11 GMT","Content-Length":"545","Etag":"W/\"221-h9plcAy/Mv8nhhxJxtovOQ\""},
+  //   "response":
+  //     "{\"public_id\":\"lmqurdsaktfcrlpvfulg\",\"version\":1456182610,\"signature\":\"ca063b29edf07c25812a39c21a067f0475356f22\",\"width\":266,\"height\":266,\"format\":\"jpg\",\"resource_type\":\"image\",\"created_at\":\"2016-02-22T23:10:10Z\",\"tags\":[],\"bytes\":26216,\"type\":\"upload\",\"etag\":\"d287e75aac034128df0803c96f7ef443\",\"url\":\"http://res.cloudinary.com/drjseeoep/image/upload/v1456182610/lmqurdsaktfcrlpvfulg.jpg\",\"secure_url\":\"https://res.cloudinary.com/drjseeoep/image/upload/v1456182610/lmqurdsaktfcrlpvfulg.jpg\",\"original_filename\":\"927605dc4ec6bf01a394640898ff01f7\"}","bytesSent":26963}
