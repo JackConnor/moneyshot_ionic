adc5ef5e2436a402318409eee10c149ba0420e48
Jack Connor <jack.connor83@gmail.com>
2016-02-22 16:24:19 -0800

    yo

--- www/js/controllers/cameraController.js
+++ www/js/controllers/cameraController.js
@@ -44,7 +44,22 @@ angular.module('cameraController', ['singlePhotoFactory', 'ngFileUpload'])
           console.log('in the callback');
           console.log(callbackImage);
           console.log("_--------------------");
-          // $scope.imageSrc = callbackImage.response;
+          $http({
+            method: "GET"
+            ,url: "http://192.168.0.11:5555/api/all/photos"
+          })
+          .then(function(photos){
+            console.log('in the callback');
+            console.log(photos);
+            var allPhotos = photos.data.reverse();
+            var allUrls = [];
+            for (var i = 0; i < allPhotos.length; i++) {
+              allUrls.push(allPhotos[i].url);
+            }
+            console.log(allUrls);
+            // $scope.testImage = allUrls[0];
+            $('.testing').attr('src', allUrls[0])
+          })
         })
       })
 
