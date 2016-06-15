6fdfb2d55fff167f8cf8491e5d287261c5bdc513
Jack Connor <jack.connor83@gmail.com>
2016-06-15 09:51:03 -0700

    testing new focus event

--- www/js/controllers/cameraController.js
+++ www/js/controllers/cameraController.js
@@ -1151,6 +1151,7 @@ angular.module('cameraController', ['singlePhotoFactory', 'ngFileUpload', 'ngCor
         $('.submitRepeat').animate({
           height: '0px'
         }, 250);
+        $('.photoNameInput').focus();
       // $timeout(function(){
       //   $('.photoNameInput').focus();
       // }, 1000);
@@ -1162,6 +1163,7 @@ angular.module('cameraController', ['singlePhotoFactory', 'ngFileUpload', 'ngCor
         $('.submitRepeat').animate({
           height: '0px'
         }, 250);
+        $('.photoNameDesc').focus();
       // $('.submitRepeat').animate({
       //   height: '0px'
       // }, 250);
